1d64604b9f2c2cb55f09d6e0fcce0c18ca9696f3
Алексей Шашин <alexshashin.2023@gmail.com>
2026-01-06 15:20:27 +0500

    созданы основные страницы и класс подключения к базе данных

---
+++ Classes/Connection.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr28.Classes
+{
+    public class Connection
+    {
+        public static MySqlDataReader Query(string query, MySqlConnection connection)
+        {
+            try
+            {
+                if(connection.State != System.Data.ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+                var command = new MySqlCommand(query, connection);
+                return command.ExecuteReader();
+            } catch(Exception)
+            {
+                return null;
+            }
+        }
+}
